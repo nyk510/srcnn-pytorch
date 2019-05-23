558829751f1c8722b31872ca3fecf80e19a45571
nyker510 <takamail53@gmail.com>
2019-05-23 23:38:51 +0900

    [update] add upscale search

--- schedule_training.sh
+++ schedule_training.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-#for final_lr in 1.0 0.2 0.1 0.05 0.01; do
-#  for lr in 0.02 0.01 0.001 0.0001; do
-#    python train.py --optimizer adabound --model bnsrcnn --lr ${lr} --final_lr ${final_lr} --batch 128 --loss mse-clop
-#  done
-#done#
-for opt in 'sgd' 'adam'; do
-for lr in 0.001 0.0005 0.0001; do
-  python train.py --optimizer ${opt} --model espcn --lr ${lr} --batch 128 --dataset bsds
-done
-done
-
+for scale in 2 3 4 5; do
+for lr in 0.01 0.001 0.0001; do
 for final_lr in 1.0 0.2 0.1 0.05 0.01; do
-  for lr in 0.02 0.01 0.001 0.0001; do
-    python train.py --optimizer adabound --model espcn --lr ${lr} --final_lr ${final_lr} --batch 128 --dataset bsds
+    python train.py \
+        --optimizer adabound \
+        --model espcn \
+        --lr ${lr} \
+        --final_lr ${final_lr} \
+        --batch 128 \
+        --dataset bsds \
+        --upscale ${scale}
   done
 done
-#
-#for opt in 'sgd' 'adam'; do
-#  for model in 'bnsrcnn' 'srcnn'; do
-#    for lr in 0.01 0.001 0.0001; do
-#      python train.py --optimizer ${opt} --model ${model} --lr ${lr} --batch 128 --loss mse-clop
-#    done
-#  done
-#done
+done
